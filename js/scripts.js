3376199ce338ce36935f587d0d570e139b45edc8
jhl-brandeis <jhl@brandeis.edu>
2025-02-01 14:10:30 -0500

    F

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,24 @@ fullscreenTarget.addEventListener('click', event=> {
         console.log(error);
     }
 });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll('.fullscreenTarget').forEach(img => {
+        img.addEventListener('click', () => {
+            if (img.requestFullscreen) {
+                img.requestFullscreen();
+            } else if (img.webkitRequestFullscreen) {
+                img.webkitRequestFullscreen();
+            }
+        });
+    });
+
+    // Open HEIC files in a new tab for Vision Pro users
+    document.querySelectorAll('a[href$=".HEIC"]').forEach(link => {
+        link.addEventListener('click', event => {
+            event.preventDefault(); // Prevent default link behavior
+            window.open(link.href, '_blank'); // Open HEIC file in a new tab
+        });
+    });
+});
